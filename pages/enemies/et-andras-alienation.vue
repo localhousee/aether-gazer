d980a96421ab5104f4092959942fab2fba7fac43
localhousee <localhousee@outlook.com>
2023-09-14 22:09:38 +0700

    fix(enemies): update `ET<Andras - Alienation>`

--- pages/enemies/et-andras-alienation.vue
+++ pages/enemies/et-andras-alienation.vue
@@ -7,18 +7,17 @@
     description="A Visbane variant that appears is Asterism ruins, it mutated after absorbing energy from the Gen-zone ruins."
     :intel="[
       'It appears in its full form without the need for the further corrosion to complete its metamorphosis. A reflection of the dark side of the stars and draped in a tattered veil, it has come to be seen as a pathfinder of the silent void.',
-      'It can launch even stronger attacks at close range, and is additionally capable of deeper levels of thought.',
-      'Covetous of the imprint Power gathered here, it poses the greatest obstacle to Modifiers attempting to explore the Gen-zone ruins.',
+      'It can launch even stronger attacks at long range, and is additionally capable of deeper levels of thought.',
+      'Covetous of the Imprint Power gathered here, it poses the greatest obstacle to Modifiers attempting to explore the Gen-zone ruins.',
     ]"
     :skills="[
       {
         name: 'Astral Burst',
-        description:
-          'Summons a ball of energy from under the enemy\'s feet, which explodes and deals damage.',
+        description: `Summons a ball of energy from under the enemy's feet, which explodes and deals damage.`,
       },
       {
         name: 'Starlight',
-        description: 'Summons a powerful ray to attack the enemy ahead',
+        description: 'Summons a powerful ray to attack the enemy ahead.',
       },
       {
         name: 'Falling Star',
