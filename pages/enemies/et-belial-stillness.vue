ff9868495462062c2b8590ee0773a19e73826e0f
localhousee <localhousee@outlook.com>
2023-09-14 22:01:45 +0700

    fix(enemy): update `ET <Belial - Stillness>`

--- pages/enemies/et-belial-stillness.vue
+++ pages/enemies/et-belial-stillness.vue
@@ -8,7 +8,7 @@
     :intel="[
       'It appears in its full form without the need for the further corrosion to complete its metamorphosis. A reflection of the dark side of the stars and draped in a tattered veil, it has come to be seen as a pathfinder of the silent void.',
       'It can launch even stronger attacks at close range, and is additionally capable of deeper levels of thought.',
-      'Covetous of the imprint Power gathered here, it poses the greatest obstacle to Modifiers attempting to explore the Gen-zone ruins.',
+      'Covetous of the Imprint Power gathered here, it poses the greatest obstacle to Modifiers attempting to explore the Gen-zone ruins.',
     ]"
     :skills="[
       {
