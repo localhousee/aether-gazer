bb0319cd3987ec53ec2c1422329393a3557605c4
Localhousee <localhousee@outlook.com>
2023-09-16 15:36:00 +0700

    feat(enemies): add new page `AGI Mecha A-01<Welding Torch>`

---
+++ pages/enemies/agi-mecha-a-01-welding-torch.vue
@@ -0,0 +1,26 @@
+<template>
+  <Enemy
+    name="AGI Mecha A-01 <Welding Torch>"
+    image="agi-mecha-a-01-welding-torch.jpg"
+    type="AGI Mecha"
+    risk="I"
+    description="A small welding torch-type AGI Mecha possessing some destructive power."
+    :intel="[
+      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Speedlght factory.',
+      'Specializes in raged attacks and control measures. Lacking a means of either escape or counterattack, dealing with it up close is pretty easy, but its threat is greater at mid- to- long-range distances.',
+      'Standing between it and its workbench is inadvisable, as it may regard you as an item in need of soldering.',
+    ]"
+    :skills="[
+      {
+        name: 'Energized Solder Strike',
+        description:
+          'Launches a long-ranged artillery strike at an enemy ahead.',
+      },
+      {
+        name: 'Charged Scattershot',
+        description:
+          'Charges up a laser cannon and fires at enemies ahead.',
+      },
+    ]"
+  />
+</template>
