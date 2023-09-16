be7759075bd2525de6f8ebb6b668741d1a0b04d8
Localhousee <localhousee@outlook.com>
2023-09-16 15:42:57 +0700

    feat(enemies): add new page `AGI Mecha Z-03<Battlefield (Broken)>`

---
+++ pages/enemies/agi-mecha-z-03-battlefield-broken.vue
@@ -0,0 +1,25 @@
+<template>
+  <Enemy
+    name="AGI Mecha Z-03 <Battlefield (Broken)>"
+    image="agi-mecha-z-03-battlefield-broken.jpg"
+    type="AGI Mecha"
+    risk="I"
+    description="A impaired humanoid AGI Mecha that still retains a strong urge to fight."
+    :intel="[
+      `It wasn't always smooth sailing in the process of developing of the AGI Mecha Legion. These impaired humanoid AGI Mecha are the failed products of the early stages of the process.`,
+      'These impaired humanoid AGI Mechas, through lacking locomotive power and firepower output, consist of a framework build to be extract design specification of the more advanced B-01. So, estimating it on the basis of that grisly exterior is guaranteed to incur a steep price.',
+      `When defeated, it emits a muffled mechanical noise from deep within its body, the meaning of which is known only to its creator, i'm afraid.`,
+    ]"
+    :skills="[
+      {
+        name: 'Searing Arc',
+        description:
+          'Raises one arm and unleashes a powerful arc at a distant enemy.',
+      },
+      {
+        name: 'Charged Collision',
+        description: 'Charges up power and rams into a distant enemy.',
+      },
+    ]"
+  />
+</template>
