21e7a2ffb4177c91a16f7994ee4e17c1a0f5a0bb
Localhousee <localhousee@outlook.com>
2023-09-16 15:31:52 +0700

    feat(enemies): add new page `AGI Mecha A-01<Hot Saw>`

---
+++ pages/enemies/agi-mecha-a-01-hot-saw.vue
@@ -0,0 +1,26 @@
+<template>
+  <Enemy
+    name="AGI Mecha A-01 <Hot Saw>"
+    image="agi-mecha-a-01-hot-saw.jpg"
+    type="AGI Mecha"
+    risk="I"
+    description="A small chainsaw-type AGI Mecha possessing some destructive power."
+    :intel="[
+      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Speedlght factory.',
+      `While fitted with chainsaws or either side for quick and efficient cutting of items, it's actually relatively quiet.`,
+      'It gets very irritable when its work with interfered with.',
+    ]"
+    :skills="[
+      {
+        name: 'Hot Saw Rush',
+        description:
+          'Brings both saw arms together and strikes forward at full force.',
+      },
+      {
+        name: 'Deadly Spin',
+        description:
+          'Charges modes and spins rapidly at the enemy for period of time.',
+      },
+    ]"
+  />
+</template>
