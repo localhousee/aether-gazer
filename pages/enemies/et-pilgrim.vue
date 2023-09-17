ac990ff856dc522321e14a79c87a3274a6af6cd2
Localhousee <localhousee@outlook.com>
2023-09-17 12:34:30 +0700

    fix(enemies): update `ET<Pilgrim>`

--- pages/enemies/et-pilgrim.vue
+++ pages/enemies/et-pilgrim.vue
@@ -4,10 +4,10 @@
     image="et-pilgrim.jpg"
     type="Visbanes"
     risk="II"
-    description="A special Encroacher Type Visbane and a fail product of the Soul Replacement Plan, it was formed when its soul and memories were corrupted by Turing's Bane Energy after being rejected by the coordinates."
+    description="A special Encroacher Type Visbane and a failed product of the Soul Replacement Plan, it was formed when its soul and memories were corrupted by Turing's Bane Energy after being rejected by the coordinates."
     :intel="[
-      'Capable of refining pure Bane Energy into a diving force by itself, its high purity level is also evidenced by its color.',
-      'It uses telekinesis to attack enemies from a distance and its abilities include (but are not limited to ) invisibility and teleportation, making it rather different from previous Encroacher Types.',
+      'Capable of refining pure Bane Energy into a diving force for itself, its high purity level is also evidenced by its color.',
+      'It uses telekinesis to attack enemies from a distance and its abilities include (but are not limited to) invisibility and teleportation, making it rather different from previous Encroacher Types.',
       'Intelligent enough to understand commands, Turing has arranged for this Visbane to assist Otis in his ongoing experiments to find coordinates.',
     ]"
     :skills="[
@@ -18,15 +18,15 @@
       },
       {
         name: 'Seismic Wave',
-        description: `Summons a circular shockwave from the under enemy's feet that inflicts Knockup on enemies on rage.`,
+        description: `Summons a circular shockwave from the under enemy's feet that inflicts Knockup on enemies in range.`,
       },
       {
         name: 'Sensory Confinement',
-        description: `Summons a high-powered matrix from the under enemy's feet, which rapidly expands to forms a deceleration filed that restricts enemies' movement.`,
+        description: `Summons a high-powered matrix from the under enemy's feet, which rapidly expands to form a deceleration field that restricts enemies' movements.`,
       },
       {
         name: 'Energy-Amassing Star',
-        description: `Flickers over to the enemy's side and form two energy orbs in front, which it explode and confuse enemies hit in the blast.`,
+        description: `Flickers over to the enemy's side and forms two energy orbs in front, which explode and confuse enemies hit in the blast.`,
       },
     ]"
   />
