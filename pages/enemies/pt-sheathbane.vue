239a6421f774281583da5a79ff0c15e95f889d52
Localhousee <localhousee@outlook.com>
2023-09-17 11:17:44 +0700

    fix(enemies): update `PT<Sheathbane>`

--- pages/enemies/pt-sheathbane.vue
+++ pages/enemies/pt-sheathbane.vue
@@ -7,7 +7,7 @@
     description="A Primal Type Visbane possessing a similar structure to that of marine life, with sharp spikes jutting out from its entire body."
     :intel="[
       `While they usually remain hidden in the ocean and are rarely sighted, their impact on marine navigation can't be ignored.`,
-      `Since there's no way to determine their population size, Modifiers must periodically be dispatch to cull them.`,
+      `Since there's no way to determine their population size, Modifiers must periodically be dispatched to cull them.`,
       `Using their spiny bodies as a weapon, they'll launch sudden attacks from under the water's surface. They must be handled with caution.`,
     ]"
     :skills="[
@@ -17,9 +17,9 @@
           'Leaps out from the water and quickly fires a laser at the enemy.',
       },
       {
-        name: 'Rocket Raid',
+        name: 'Surface Tension',
         description:
-          'Folds its body into a special form, then rapidly fires into the air and attacks the enemies during its descent.',
+          'Folds its body into a special form, then rapidly charges forward at the distant enemy to attack.',
       },
     ]"
   />
