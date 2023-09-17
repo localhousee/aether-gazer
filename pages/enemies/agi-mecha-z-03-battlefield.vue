8cb9624aa0acf8a898a646acb740e52506b4207b
Localhousee <localhousee@outlook.com>
2023-09-17 16:36:36 +0700

    feat(enemies): add new page `AGI Mecha Z-03<Battlefield>`

---
+++ pages/enemies/agi-mecha-z-03-battlefield.vue
@@ -0,0 +1,30 @@
+<template>
+  <Enemy
+    name="AGI Mecha Z-03 <Battlefield>"
+    image="agi-mecha-z-03-battlefield.jpg"
+    type="AGI Mecha"
+    risk="II"
+    description="This towering form features an anthropomorphic structures and is capable of impressive maneuverability."
+    :intel="[
+      'With reduced armor resistance, firepower and energy consumption make it more strategic when used to capture cities in large-scale war',
+      'Trading in some of its heavy firepower equipment for an advanced humanoid structure both increases its mobility and signification reduces the required material costs and production time, making it ready for mass production.',
+      'They operate quite orderly as a collective, even their attack movements are highly precise.',
+    ]"
+    :skills="[
+      {
+        name: 'Strike Operation',
+        description:
+          'Activates a jet propulsion system to rush towards the target and send them flying with a sharp weapon.',
+      },
+      {
+        name: 'Fixed Strike',
+        description: 'Charges up and sprint toward.',
+      },
+      {
+        name: 'Armed Evolution',
+        description:
+          'Enter Enhanced Mode, increasing skill speed and shortening attack intervals.',
+      },
+    ]"
+  />
+</template>
