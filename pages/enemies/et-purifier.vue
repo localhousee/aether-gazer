7615f753b9f24af94f9129f1f2b2542c48ffdaec
Localhousee <localhousee@outlook.com>
2023-09-17 12:30:33 +0700

    fix(enemies): update `ET<Purifier>`

--- pages/enemies/et-purifier.vue
+++ pages/enemies/et-purifier.vue
@@ -4,10 +4,10 @@
     image="et-purifier.jpg"
     type="Visbanes"
     risk="II"
-    description="A special Encroacher Type Visbane and a fail product of the Soul Replacement Plan, it was formed when its soul and memories were corrupted by Turing's Bane Energy after being rejected by the coordinate."
+    description="A special Encroacher Type Visbane and a failed product of the Soul Replacement Plan, it was formed when its soul and memories were corrupted by Turing's Bane Energy after being rejected by the coordinates."
     :intel="[
-      'Capable of refining pure Bane Energy into a diving force by itself, its high purity level is also evidenced by its color.',
-      'It attacks using dual blades and its abilities include (but are not limited to ) flickering between locations and charging energy forms, making it rather different from previous Encroacher Types.',
+      'Capable of refining pure Bane Energy into a diving force for itself, its high purity level is also evidenced by its color.',
+      'It attacks using dual blades and its abilities include (but are not limited to) flickering between locations and changing energy forms, making it rather different from previous Encroacher Types.',
       'Intelligent enough to understand commands, Turing has arranged for this Visbane to assist Otis in his ongoing experiments to find coordinates.',
     ]"
     :skills="[
@@ -19,7 +19,7 @@
       {
         name: 'Extradimensional Shift',
         description:
-          'Flickers over the enemy and deals two consecutive attacks.',
+          'Flickers over near the enemy and deals two consecutive attacks.',
       },
       {
         name: 'Purifying Propulsion',
@@ -28,7 +28,7 @@
       },
       {
         name: 'Deathly Silence',
-        description: `Coalesces high-purity energy into an orb and fires it at the enemy, where it explodes and forms a deceleration filed the restricts enemies' movement.`,
+        description: `Coalesces high-purity energy into an orb and fires it at the enemy, where it explodes and forms a deceleration field the restricts enemies' movements.`,
       },
     ]"
   />
