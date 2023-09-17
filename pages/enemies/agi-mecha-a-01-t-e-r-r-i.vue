cf2447dfc0491253e320dde178965101d6aa19a9
Localhousee <localhousee@outlook.com>
2023-09-17 16:35:19 +0700

    feat(enemies): add new page `Agi Mecha A-01<T.E.R.R.I>`

---
+++ pages/enemies/agi-mecha-a-01-t-e-r-r-i.vue
@@ -0,0 +1,26 @@
+<template>
+  <Enemy
+    name="AGI Mecha A-01 <N.O.L.A.N>"
+    image="agi-mecha-a-01-n-o-l-a-n.jpg"
+    type="AGI Mecha"
+    risk="I"
+    description="Made by Aether Gazer, the main function of these AGI Mechas is hospitality. Equipped with internal heating and cooling units, they can often hot and cold beverage to guests and staff."
+    :intel="[
+      `A type of AGI Mecha utilized by Aether Gazer. Designed by Ookuninushi, who also provided a portion of the technology, and build by the Cryptic, it's one of the Aether Gazer's proudest works and boasts even more advanced capabilities than the other AGI Mechas on the market.`,
+      'To allow them to remain steady while handing drinks or food to guests, they were designed to move by hovering.',
+      'These AGI Mecha were made extremely dangerous by Bane Energy corrosion.',
+    ]"
+    :skills="[
+      {
+        name: 'Social Distancing',
+        description:
+          'Raises its mechanical arm and launches an electric arc forward at the distant enemy.',
+      },
+      {
+        name: 'Full Devices',
+        description:
+          'Raises its mechanical arm and cleanly cuts through obstacles in a circle around it.',
+      },
+    ]"
+  />
+</template>
