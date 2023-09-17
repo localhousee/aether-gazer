baa5035c5a6d65eba72f4b30ef2a1b8c81c9918d
Localhousee <localhousee@outlook.com>
2023-09-17 16:35:55 +0700

    feat(enemies): add new page `AGI Mecha A-06<Glory>`

---
+++ pages/enemies/agi-mecha-a-06-glory.vue
@@ -0,0 +1,30 @@
+<template>
+  <Enemy
+    name="AGI Mecha A-06 <Glory>"
+    image="agi-mecha-a-06-glory.jpg"
+    type="AGI Mecha"
+    risk="I"
+    description="The star of the production line, its solid body is outfitted with a variety of tools."
+    :intel="[
+      'The Glory AGI Mecha comes loaded with a variety of equipment used in industrial production.Its sturdy form allows it to undertake all sort pf onerous tasks.',
+      'Its relatively strong torso and limbs allow it to carry out grueling production work.',
+      'They come equipped with different arms according to their specific model, such as cutting saws or electric drills...',
+    ]"
+    :skills="[
+      {
+        name: 'Chainsaw Sweep',
+        description:
+          'Activates its chainsaw module and delivers a sweeping attack to an enemy.',
+      },
+      {
+        name: 'Drill Spark',
+        description: 'Activates its drill module and jabs at an enemy.',
+      },
+      {
+        name: 'Glorious Mission',
+        description:
+          'Activates its both modules at once and rams into an enemy.',
+      },
+    ]"
+  />
+</template>
