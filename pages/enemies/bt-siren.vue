119b8bb873398e2c452d146ef2db958332b6b433
Localhousee <localhousee@outlook.com>
2023-09-18 09:30:32 +0700

    fix(enemies): update `BT<Siren>`

--- pages/enemies/bt-siren.vue
+++ pages/enemies/bt-siren.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const description = `A Behemoth Visbane Type that appears near the Peripheral Sea, this is the true form of the "demoness" often spotted at sea. He hides his massive form under the ocean's surface, when it quietly observes everything that happens there.`;
+const description = `A Behemoth Type Visbane that appears near the Peripheral Sea, this is the true form of the "demoness" often spotted at sea. He hides his massive form under the ocean's surface, where it quietly observes everything that happens there.`;
 </script>
 <template>
   <Enemy
@@ -9,8 +9,8 @@ const description = `A Behemoth Visbane Type that appears near the Peripheral Se
     risk="III"
     :description="description"
     :intel="[
-      'Capable of manipulating the ocean currents and the controlling storms, it can rapidly move around the ocean, despite the large body.',
-      'He can tamper with the reality of the Source Layer by the corrupting the data of the Source Layer, forming illusory bubble worlds. The space contained within these bubble worlds disappears as soon as they burst.',
+      'Capable of manipulating the ocean currents and the controlling storms, it can rapidly move around the ocean, despite his large body.',
+      'He can tamper with the reality of the Surface Layer by corrupting the data of the Source Layer, forming illusory bubble worlds. The space contained within these bubble worlds disappears as soon as they burst.',
       `Like a sailor enraptured by a siren's song, who only comes to their senses when their ship runs aground, it's only when the bubble bursts that the people contained within it become aware of their imminent doom.`,
     ]"
     :skills="[
@@ -22,7 +22,7 @@ const description = `A Behemoth Visbane Type that appears near the Peripheral Se
       {
         name: 'Dream Rush',
         description:
-          'Siren summons numerous water projectiles and sends them hurting forward the enemy ahead.',
+          'Siren summons numerous water projectiles and sends them hurtling toward the enemy ahead.',
       },
       {
         name: 'Sea-Piercing Torrent',
