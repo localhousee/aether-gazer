40b72bfe73d66a742d9e4ce09982029a89032434
Localhousee <localhousee@outlook.com>
2023-09-18 07:01:26 +0700

    feat(enemies): add new page `COI<Security>`

---
+++ pages/enemies/coi-security.vue
@@ -0,0 +1,28 @@
+<template>
+  <Enemy
+    name="COI <Security>"
+    image="coi-security.jpg"
+    type="Humanoid"
+    risk="I"
+    description="Security guards wearing light combat fatigues."
+    :intel="[
+      'Low-level staff employed by Iron Security, some were selected directly from Children of Iron while others were recruited after the company founded.',
+      'Equipped with ranged firearms to keep enemies at bay, their specific proficiencies are unknown. It is also entirely possible that they will abandon their armor and flee from combat.',
+      `While their general combat ability isn't much to speak of, the more outstanding among them will be promoted to squad leaders.`,
+    ]"
+    :skills="[
+      {
+        name: 'Ranged Snipe',
+        description: 'Crouches down and take a snipe at an enemy ahead.',
+      },
+      {
+        name: 'Gunshot',
+        description: 'Draws a gun and fires at an enemy ahead',
+      },
+      {
+        name: 'Rough Thwack',
+        description: 'Roughly swings a riffle at an enemy.',
+      },
+    ]"
+  />
+</template>
