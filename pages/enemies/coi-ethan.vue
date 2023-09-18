cbf8badd996b13974a2397894fea968e9507b658
Localhousee <localhousee@outlook.com>
2023-09-18 07:04:10 +0700

    feat(enemies): add new page `COI<Ethan>`

---
+++ pages/enemies/coi-ethan.vue
@@ -0,0 +1,15 @@
+<template>
+  <Enemy
+    name="COI <Ethan>"
+    image="coi-ethan.jpg"
+    type="Humanoid"
+    risk="III"
+    description="The handsome young leader of the Children of Iron. The majority of his body has been mechanized and he always wears an easy smile on his face."
+    :intel="[
+      'A man who lost all his memories and woke up in some ancient ruins on a pile of rubble and spare parts. 80% of his body tissues have been changed to machinery.',
+      'The current leader of Children of Iron, he is actively searching all over for clues regarding the current state of his body.',
+      'Though proficient with various weapon types, his indolent behavior makes him quite puzzling to others.',
+    ]"
+    :skills="[]"
+  />
+</template>
