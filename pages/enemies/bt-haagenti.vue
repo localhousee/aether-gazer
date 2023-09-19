30973cf4cb1d802cd2f88b24a7daf6e00176e9c4
Localhousee <localhousee@outlook.com>
2023-09-19 22:09:42 +0700

    fix(enemies): update `BT<Haagenti>`

--- pages/enemies/bt-haagenti.vue
+++ pages/enemies/bt-haagenti.vue
@@ -7,13 +7,13 @@
     description="The two strong arms on either side of this massive body are its greatest threat."
     :intel="[
       `Among all observed Visbane, the Behemoth Visbane - codenamed &quot;Haagenti&quot; - is quite rare. The caution of its formation is still unknown.`,
-      'A special individual speculated to have been formed from the massive consumption of Source Layer material by a Visbane, it has a enormous body and destruction power for surpassing that of original Visbane.',
-      'A blow from its two fists can directly tear open a channel to the Surface Layer. Its acts on purely destructive will, making a huge threat and a primary target for removal.',
+      'A special individual speculated to have been formed from the massive consumption of Source Layer material by a Visbane, it has a enormous body and destructive power far surpassing that of ordinary Visbanes.',
+      'A blow from its two fists can directly tear open a channel to the Surface Layer. Its acts on purely destructive will, making it a huge threat and a primary target for removal.',
     ]"
     :skills="[
       {
         name: 'Hyperspace Strike',
-        description: `Haagenti appears by an enemy's side, raises its arm high, then brings them back down again in a heavy blow.`,
+        description: `Haagenti appears by an enemy's side, raises its arms high, then brings them back down again in a heavy blow.`,
       },
       {
         name: 'Burst Spark',
