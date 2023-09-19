13be2d4ca8775577d654b0a31f649d4dae153c0a
Localhousee <localhousee@outlook.com>
2023-09-19 22:10:07 +0700

    fix(enemies): update `CT<Halphas>`

--- pages/enemies/ct-halphas.vue
+++ pages/enemies/ct-halphas.vue
@@ -6,7 +6,7 @@
     risk="III"
     description="A six-legged crawling Visbane covered head to toe in Transzist ore. Its prototype was a hideous spider whose body was outfitted with heavy firepower AGI Mecha armor."
     :intel="[
-      'A Cognizant Visbane that appeared in the Source Layer of the Neuhansa Districts, it was heavily affected by the industrialization on the surface. It has a massive mechanical body.',
+      'A Cognizant Visbane that appeared in the Source Layer of the Neuhansa District, it was heavily affected by the industrialization on the surface. It has a massive mechanical body.',
       'Fully capable of thought and speech, its behavior is more intensely purposeful than merely driven by pure destruction.',
       'Its entire body is equipped with a number of frightening heavy firepower weapons. From the moment it enters combat, it will level the battlefield with its explode firepower.',
     ]"
@@ -19,7 +19,7 @@
       {
         name: 'Tracking Artillery Strike',
         description:
-          'Halphas unleashes s tracking ray that confirms the target position before launching a powerful artillery strike.',
+          'Halphas unleashes a tracking ray that confirms the target position before launching a powerful artillery strike.',
       },
     ]"
   />
