e7270fb9f3f24267fbcf1815d5450fb1b61e45dd
Localhousee <localhousee@outlook.com>
2023-09-19 22:08:36 +0700

    fix(enemies): update `Snake God<Orochi>`

--- pages/enemies/snake-god-orochi.vue
+++ pages/enemies/snake-god-orochi.vue
@@ -7,10 +7,10 @@
     description="A mature and coquettish woman with several huge, ferocious snakes lurking behind her, ready to strike at any moment."
     :intel="[
       `Once one of the five mountain gods that ruled over Sasanami and the object of the Miyahebi family's worship, she was sealed away a hundred years ago.`,
-      'She was summoned and awake n by Miyahebi Syuu and has secretly been conducting experiments combining humans and Visbanes in the Miyahebi family manor.',
-      'Arrogant and cruel, she likes to toy with her preys. She can use multiple katanas to launch a tidal wave of continuous attacks.' +
+      'She was summoned and awaken by Miyahebi Syuu and has secretly been conducting experiments combining humans and Visbanes in the Miyahebi family manor.',
+      'Arrogant and cruel, she likes to toy with her prey. She can use multiple katanas to launch a tidal wave of continuous attacks.' +
         '<br>' +
-        'Additionally, she has the ability to command Visbane snakes. Orochi who came to the battlefield in the form of a a mountain deity, portends a calamity that will return to this land once again.',
+        'Additionally, she has the ability to command Visbane snakes. Orochi, who came to the battlefield in the form of a a mountain deity, portends a calamity that will return to this land once again.',
     ]"
     :skills="[
       {
