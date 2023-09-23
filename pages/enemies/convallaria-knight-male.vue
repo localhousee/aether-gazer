8e205138f9830ec0648d52fdf4624d14146dc969
Localhousee <localhousee@outlook.com>
2023-09-23 22:25:37 +0700

    fix(enemies): update `Convallaria Knight<Male>`

--- pages/enemies/convallaria-knight-male.vue
+++ pages/enemies/convallaria-knight-male.vue
@@ -2,17 +2,17 @@
   <Enemy
     name="Convallaria Knight <Male>"
     image="convallaria-knight-Male.jpg"
-    type="Humannoid"
+    type="Humanoid"
     risk="I"
     description="A male member of the Knights of Convallaria, he's mainly responsible for ensuring the safety of the Astral Council and Stellaris Temple."
     :intel="[
-      'As guardian of law and order in Omorfies, members receive basic combat training. Most are given melee weapons, but some members are equipped with longbows.',
-      'Many members have an admiration for the arts, but were unable to become artists themselves, so they resolved to protect the art instead.',
-      'The long-term peace enjoyed by Omorfies had led some members to neglect their training, resulting in a decease in their actual combat abilities.',
+      'As guardians of law and order in Omorfies, members receive basic combat training. Most are given melee weapons, but some special members are equipped with longbows.',
+      'Many members have an admiration for the arts, but were unable to become artists themselves, so they resolved to protect the arts instead.',
+      'The long-term peace enjoyed by Omorfies had led some members to neglect their training, resulting in a decrease in their actual combat abilities.',
     ]"
     :skills="[
       {
-        name: 'Shield Burst',
+        name: 'Shield Rush',
         description: 'Raises his shield and slams into the enemy ahead.',
       },
       {
