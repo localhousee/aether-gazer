913d81de189a0737c4e0c1ec3fc1f311d1e7762b
Localhousee <localhousee@outlook.com>
2023-09-23 22:25:18 +0700

    fix(enemies): update `Convallaria Commander<Athena>`

--- pages/enemies/convallaria-commander-athena.vue
+++ pages/enemies/convallaria-commander-athena.vue
@@ -2,13 +2,13 @@
   <Enemy
     name="Convallaria Commander <Athena>"
     image="convallaria-commander-athena.jpg"
-    type="Humannoid"
+    type="Humanoid"
     risk="III"
-    description="The commander of the Knights of Convallaria and the adopted granddaughter of the Colleen Pallas whose family has been members of the Knights of Convallaria for generations. And Athena as has always strived to be an outstanding of knight, she is upright and chivalrous, upholding a code of justice and loyalty."
+    description="The commander of the Knights of Convallaria and the adopted granddaughter of the Colleen Pallas whose family has been members of the Knights of Convallaria for generations. And Athena also as has always strived to be an outstanding knight, she is upright and chivalrous, upholding a code of justice and loyalty."
     :intel="[
       'The order of Omorfies could not be broken, and the honor of the Knights of Convallaria could not be sullied. In carrying these heavy burdens, Athena did not allow herself to slacken for a moment.',
-      `Athena's sense of responsibility set apart her from the crowd, and she was surrounded by countless admirers and even worshipers. She found it hard to find friends who would treat her as a true equal.`,
-      `Athena's adopted granddaughter had always served as a role model for her. She would work to make real her ideals and expel any invaders who would threaten Omorfies. However, even the greatest of power may collapse following a loss of faith...`,
+      `Athena's sense of responsibility set her apart from the crowd, and she was surrounded by countless admirers and even worshippers. She found it hard to find friends who would treat her as a true equal.`,
+      `Athena's adopted grandmother had always served as a role model for her. She would work to make real her ideals and expel any invaders who would threaten Omorfies. However, even the greatest of power may collapse following a loss of faith...`,
     ]"
     :skills="[
       {
@@ -19,7 +19,7 @@
       {
         name: 'Swaying Sound',
         description:
-          'Athena launches a blast of sound waves the drift forward and explode shortly after touching an enemy, dealing damage to enemies in a circular area and putting them to sleep.',
+          'Athena launches a blast of sound waves that drift forward and explode shortly after touching an enemy, dealing damage to enemies in a circular area and putting them to sleep.',
       },
       {
         name: 'Dazzling Chord',
