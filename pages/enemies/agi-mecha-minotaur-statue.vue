9c2734b8b4825ebef6ddcef8d4a1e2854176d4e9
Localhousee <localhousee@outlook.com>
2023-09-23 22:22:49 +0700

    fix(enemies): update `AGI Mecha<Minotaur Statue>`

--- pages/enemies/agi-mecha-minotaur-statue.vue
+++ pages/enemies/agi-mecha-minotaur-statue.vue
@@ -4,10 +4,10 @@
     image="agi-mecha-minotaur-statue.jpg"
     type="AGI Mecha"
     risk="II"
-    description="A security AGI Mecha featuring a tall, muscular physique, it's roundly landed by the Knights of Convallaria as a potent deterrent as well as for its massive, statuesque build."
+    description="A security AGI Mecha featuring a tall, muscular physique, it's roundly lauded by the Knights of Convallaria as a potent deterrent as well as for its massive, statuesque build."
     :intel="[
-      `To lighten the Knight of Convallaria's security load, Stellaris Academy decided to flex its own muscles. Following a year's worth of joint research, it development a series of first-rate AGI Mecha, including the Minotaur, which integrated power, technology, and art into one mecha.`,
-      `Don't be fooled by its hulking appearance -it's enhanced propulsion system makes it more agile than your average AGI Mecha.`,
+      `To lighten the Knight of Convallaria's security load, Stellaris Academy decided to flex its own muscles. Following a year's worth of joint research, it developed a series of first-rate AGI Mecha, including the Minotaur, which integrated power, technology, and art into one mecha.`,
+      `Don't be fooled by its hulking appearance - its enhanced propulsion system makes it more agile than your average AGI Mecha.`,
       'This series of AGI Mecha is currently placed at the academy entrance as well as in front of the storeroom where some important items are kept. The formidable deterrent power it exudes is enough to make would-be bandits think twice about approaching.',
     ]"
     :skills="[
@@ -18,12 +18,12 @@
       },
       {
         name: 'Banishing Axe',
-        description: `Raises its giant axe high and repeatedly pounds the ground with it. The axe's second impact sned out a shockwave that follows the enemy.`,
+        description: `Raises its giant axe up high and repeatedly pounds the ground with it. The axe's second impact sends out a shockwave that follows the enemy.`,
       },
       {
         name: 'Blistering Pursuit',
         description:
-          'Raises its giant axe high and, then send out three shockwaves that purse and deal damage to the enemy.',
+          'Raises its giant axe up high, then sends out three shockwaves that pursue and deal damage to the enemy.',
       },
     ]"
   />
