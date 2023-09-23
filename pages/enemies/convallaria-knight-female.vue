59ec12d478deda2f23e59f2d7891505ef9e3f04f
Localhousee <localhousee@outlook.com>
2023-09-23 22:25:28 +0700

    fix(enemies): update `Convallaria Knight<Female>`

--- pages/enemies/convallaria-knight-female.vue
+++ pages/enemies/convallaria-knight-female.vue
@@ -2,13 +2,13 @@
   <Enemy
     name="Convallaria Knight <Female>"
     image="convallaria-knight-female.jpg"
-    type="Humannoid"
+    type="Humanoid"
     risk="I"
     description="A female member of the Knights of Convallaria, she's mainly responsible for ensuring the safety of the Astral Council and Stellaris Temple."
     :intel="[
-      'As guardian of law and order in Omorfies, members receive basic combat training. Most are given melee weapons, but some members are equipped with longbows.',
-      'Many members have an admiration for the arts, but were unable to become artists themselves, so they resolved to protect the art instead.',
-      'The long-term peace enjoyed by Omorfies had led some members to neglect their training, resulting in a decease in their actual combat abilities.',
+      'As guardians of law and order in Omorfies, members receive basic combat training. Most are given melee weapons, but some special members are equipped with longbows.',
+      'Many members have an admiration for the arts, but were unable to become artists themselves, so they resolved to protect the arts instead.',
+      'The long-term peace enjoyed by Omorfies had led some members to neglect their training, resulting in a decrease in their actual combat abilities.',
     ]"
     :skills="[
       {
