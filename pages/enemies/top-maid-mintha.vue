50b366b46c06c941895103a5457ae646d6573e63
Localhousee <localhousee@outlook.com>
2023-09-23 22:25:56 +0700

    fix(enemies): update `Top Maid<Mintha>`

--- pages/enemies/top-maid-mintha.vue
+++ pages/enemies/top-maid-mintha.vue
@@ -2,13 +2,13 @@
   <Enemy
     name="Top Maid <Mintha>"
     image="top-maid-mintha.jpg"
-    type="Humannoid"
+    type="Humanoid"
     risk="II"
     description="A puppet that is the pride and joy of Hades. Driven by Divine Power, she is possesses a certain level of sentience and can thus operate even when Hades is not with her."
     :intel="[
       `Under the operation of Hades, the puppet's powerful armaments serve as fearsome weapons during combat. On a average day, however, the puppet serves as a maid for the Society of Muses, where she takes care of members' everyday needs.`,
-      'In their free times, the two maids wander through the private library of the Society of Muse, learning more about humanity.',
-      'The cautious and prudent Mintha spares no effort to eliminate any factors that might disrupt the order of the Society and shows no mercy to intruders within it. The only time her gentle side shows is where she faces Oneiroi.',
+      'In their free times, the two maids wander through the private library of the Society of Muses, learning more about humanity.',
+      'The cautious and prudent Mintha spares no effort to eliminate any factors that might disrupt the order of the Society and shows no mercy to intruders within it. The only time her gentle side shows is when she faces Oneiroi.'
     ]"
     :skills="[
       {
