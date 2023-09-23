b96c4603220d66afe70a204605e78272e30e08c7
Localhousee <localhousee@outlook.com>
2023-09-23 22:24:07 +0700

    fix(enemies): update `COI<Elite Guard>`

--- pages/enemies/coi-elite-guard.vue
+++ pages/enemies/coi-elite-guard.vue
@@ -6,23 +6,23 @@
     risk="I"
     description="Boasts a strong build and wears heavy protective equipment."
     :intel="[
-      'Low-level staff employed by Iron Security, some were selected directly from Children of Iron while others were recruited after the company founded.',
+      'Low-level staff employed by Iron Security, some were selected directly from Children of Iron while others were recruited after the company was founded.',
       'Equipped with a sword and shield used at close range to keep enemies at bay, their specific proficiencies are unknown. It is also entirely possible that they will abandon their armor and flee from combat.',
       `While their general combat ability isn't much to speak of, the more outstanding among them will be promoted to squad leaders.`,
     ]"
     :skills="[
       {
         name: 'Shield Charge',
-        description: ' Raises their shield and charges into an enemy ahead.',
+        description: 'Raises their shield and charges into an enemy ahead.',
       },
       {
         name: 'Defensive Stance',
         description:
-          ' Raises their shield to defend against incoming enemy damage. With its focus concentrated at the front, this move can be easily interrupted from behind.',
+          'Raises their shield to defend against incoming enemy damage. With its focus concentrated at the front, this move can be easily interrupted from behind.',
       },
       {
         name: 'Pumped Up',
-        description: ' Assumes an attacking stance to boost their ATK.',
+        description: 'Assumes an attacking stance to boost their ATK.',
       },
     ]"
   />
