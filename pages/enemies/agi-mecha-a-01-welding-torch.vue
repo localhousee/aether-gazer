b77eaf5c01f97800fdefb88b8d876acded8d9da8
Localhousee <localhousee@outlook.com>
2023-09-24 07:51:55 +0700

    fix(enemies): update `AGI Mecha A-01<Welding Torch>`

--- pages/enemies/agi-mecha-a-01-welding-torch.vue
+++ pages/enemies/agi-mecha-a-01-welding-torch.vue
@@ -6,15 +6,15 @@
     risk="I"
     description="A small welding torch-type AGI Mecha possessing some destructive power."
     :intel="[
-      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Speedlght factory.',
-      'Specializes in raged attacks and control measures. Lacking a means of either escape or counterattack, dealing with it up close is pretty easy, but its threat is greater at mid- to- long-range distances.',
+      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Spealght factory.',
+      'Specializes in raged attacks and control measures. Lacking a means of either escape or counterattack, dealing with it up close is pretty easy, but its threat is greater at mid- to long-range distances.',
       'Standing between it and its workbench is inadvisable, as it may regard you as an item in need of soldering.',
     ]"
     :skills="[
       {
         name: 'Energized Solder Strike',
         description:
-          'Launches a long-ranged artillery strike at an enemy ahead.',
+          'Launches a long-range artillery strike at an enemy ahead.',
       },
       {
         name: 'Charged Scattershot',
