0818eaddcf66d4c31864a8f893198e483cefb6ad
Localhousee <localhousee@outlook.com>
2023-09-24 07:52:40 +0700

    fix(enemies): update `AGI Mecha Z-01<Trust>`

--- pages/enemies/agi-mecha-z-01-trust.vue
+++ pages/enemies/agi-mecha-z-01-trust.vue
@@ -6,9 +6,9 @@
     risk="I"
     description="With an exterior as solid as an iron barrel, it's a practically unstoppable on the battlefield."
     :intel="[
-      'Combat performance tests have shown that its compatibility, armor resistance, and firepower output are all vastly superior to other AGI Mechas of the same class. Though its exterior is covered with sturdy, nearly seamless armor, the propulsion system on its back allows it to maneuver with astonishing speed, accompanied by only a monetary functional overload.',
+      'Combat performance tests have shown that its compatibility, armor resistance, and firepower output are all vastly superior to other AGI Mechas of the same class. Though its exterior is covered with sturdy, nearly seamless armor, the propulsion system on its back allows it to maneuver with astonishing speed, accompanied by only a momentary functional overload.',
       'This state-of-the-art technology requires significantly reduced material costs and production time, making the Z-01 Trust war AGI Mecha ready for mass production.',
-      `Even a formidable AGI Mecha like this one has a weak spot - Just by attacking one critical point, Z-01's steel shell can be broken with ease, however...tho this day, no one has successfully attempted this.`,
+      `Even a formidable AGI Mecha like this one has a weak spot - Just by attacking one critical point, Z-01's steel shell can be broken with ease, however... to this day, no one has successfully attempted this.`,
     ]"
     :skills="[
       {
@@ -19,7 +19,7 @@
       {
         name: 'Electric Rush',
         description:
-          'Expands its shield and rushes at an enemy before swings a charged mechanical arm at them in a sweeping attack.',
+          'Expands its shield and rushes at an enemy before swinging a charged mechanical arm at them in a sweeping attack.',
       },
       {
         name: 'Power of Trust',
