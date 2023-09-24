6b1f957fa05bcb6e7efa9870d0db86579b652191
Localhousee <localhousee@outlook.com>
2023-09-24 07:52:06 +0700

    fix(enemies): update `AGI Mecha A-02<Patroller>`

--- pages/enemies/agi-mecha-a-02-patroller.vue
+++ pages/enemies/agi-mecha-a-02-patroller.vue
@@ -6,9 +6,9 @@
     risk="I"
     description="A small patrol-type AGI Mecha that uses a laser emitted from a device in its head to scan's visitor's identities."
     :intel="[
-      'As an AGI Mecha responsible for detection within Spealght Industries, its main task pertain to detection and maintaining order.',
+      'As an AGI Mecha responsible for detection within Spealght Industries, its main tasks pertain to detection and maintaining order.',
       'It will issue an alert at the first sign of any hidden danger that may threaten the safety of the factory.',
-      'Due to the excessive sensitivity of the system on board to the surrounding environment, some of its combat capabilities have been reduced to pervert unintended damage.',
+      'Due to the excessive sensitivity of the system on board to the surrounding environment, some of its combat capabilities have been reduced to prevent unintended damage.',
     ]"
     :skills="[
       {
