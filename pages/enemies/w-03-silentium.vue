06abeceb0e88163ea5380d42170c0cc84b098752
Localhousee <localhousee@outlook.com>
2023-09-24 07:53:27 +0700

    fix(enemies): update `W-03<Silentium>`

--- pages/enemies/w-03-silentium.vue
+++ pages/enemies/w-03-silentium.vue
@@ -4,10 +4,10 @@
     image="w-03-silentium.jpg"
     type="AGI Mecha"
     risk="I"
-    description="Development to blend in with the elegant environment of Stellaris Academy, this security AGI Mecha possesses the aesthetically-pleasing bearing of a statue. Even through the students and city residents are aware of the existence of this type of mecha, they still occasionally find themselves startled by its sudden movements !"
+    description="Developed to blend in with the elegant environment of Stellaris Academy, this security AGI Mecha possesses the aesthetically-pleasing bearing of a statue. Even though the students and city residents are aware of the existence of this type of mecha, they still occasionally find themselves startled by its sudden movements!"
     :intel="[
       'With its superior detection capabilities and maneuverability, this type of AGI Mecha is normally used for surveillance and to warn of ill-intentioned intruders.',
-      'To ensure its maneuverability, the designers were repeatedly forced to reduce its weight untill the original design criteria could be met.',
+      'To ensure its maneuverability, the designers were repeatedly forced to reduce its weight until the original design criteria could be met.',
       `While it may not have stand-out combat capabilities, its excellent detection system can instantaneously pick up signs of danger when it's carrying out patrol missions.`,
     ]"
     :skills="[
@@ -19,7 +19,7 @@
       {
         name: 'Silent Darkness',
         description:
-          'Unleashes a circular energy filed around it that slows enemies within it.',
+          'Unleashes a circular energy field around it that slows enemies within it.',
       },
     ]"
   />
