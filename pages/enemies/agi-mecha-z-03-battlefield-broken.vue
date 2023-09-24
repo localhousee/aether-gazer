ae44ac374ffab6093c0f05f95f5f3726c3126002
Localhousee <localhousee@outlook.com>
2023-09-24 07:53:06 +0700

    fix(enemies): update `AGI Mecha Z-03<Battlefield (Broken)>`

--- pages/enemies/agi-mecha-z-03-battlefield-broken.vue
+++ pages/enemies/agi-mecha-z-03-battlefield-broken.vue
@@ -7,8 +7,8 @@
     description="A impaired humanoid AGI Mecha that still retains a strong urge to fight."
     :intel="[
       `It wasn't always smooth sailing in the process of developing of the AGI Mecha Legion. These impaired humanoid AGI Mecha are the failed products of the early stages of the process.`,
-      'These impaired humanoid AGI Mechas, through lacking locomotive power and firepower output, consist of a framework build to be extract design specification of the more advanced B-01. So, estimating it on the basis of that grisly exterior is guaranteed to incur a steep price.',
-      `When defeated, it emits a muffled mechanical noise from deep within its body, the meaning of which is known only to its creator, i'm afraid.`,
+      'These impaired humanoid AGI Mechas, through lacking in locomotive power and firepower output, consist of a framework built to the exact design specification of the more advanced B-01. So, estimating it on the basis of that grisly exterior is guaranteed to incur a steep price.',
+      `When defeated, it emits a muffled mechanical noise from deep within its body, the meaning of which is known only to its creator, I'm afraid.`,
     ]"
     :skills="[
       {
