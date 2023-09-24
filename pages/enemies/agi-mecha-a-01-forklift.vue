0a99160b145114c9f903a56f0a5db90bfeed32db
Localhousee <localhousee@outlook.com>
2023-09-24 07:50:52 +0700

    fix(enemies): update `AGI Mecha A-01<Forklift>`

--- pages/enemies/agi-mecha-a-01-forklift.vue
+++ pages/enemies/agi-mecha-a-01-forklift.vue
@@ -4,11 +4,11 @@
     image="agi-mecha-a-01-forklift.jpg"
     type="AGI Mecha"
     risk="I"
-    description="A compact AGI Mecha capable of shuttling back and forth though narrow spaces to carry out tasks."
+    description="A compact forklift AGI Mecha capable of shuttling back and forth through narrow spaces to carry out tasks."
     :intel="[
-      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Speedlght factory.',
-      'Despite being the most under-appreciate of the factory staff, it still works diligently to complete its daily tasks.',
-      `It can occasionally be spotted in the factory's corner silently practicing its hauling skills on scap containers.`,
+      'A low-level AGI Mecha that is mainly responsible for transport and other assorted tasks in the Spealght factory.',
+      'Despite being the most under-appreciated of the factory staff, it still works diligently to complete its daily tasks.',
+      `It can occasionally be spotted in the factory's corner silently practicing its hauling skills on scrap containers.`,
     ]"
     :skills="[
       {
@@ -19,7 +19,7 @@
       {
         name: 'Shovel Bash',
         description:
-          'Both arms shovel violently forward, knocking enemies its in path into the air.',
+          'Both arms shovel violently forward, knocking enemies in its path into the air.',
       },
     ]"
   />
